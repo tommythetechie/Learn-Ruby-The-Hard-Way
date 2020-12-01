3a60a817ec6cf3a86beb5d5c971684762bb8de64
Tommy Chapman <Tommy.chapman@hotmail.com>
2020-11-30 18:42:32 -0600

    update to ex7
    update to ex7

---
+++ ex7.rb
@@ -0,0 +1,34 @@
+#This line writes a string
+puts "Mary had a little lamb."
+
+#this line writes a string and inserts a variable into it for the string snow
+puts "Its fleece was white as #{'snow'}."
+
+#this line writes a string too
+puts "And everywhere that Mary went."
+
+#this line writes the string thats a period and multiplies it 10 times
+puts "." * 10 # what'd that do?
+
+#these lines create variables with strings in them with a letter
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+
+
+#watch that print vs. puts on this line what's it do?
+#this line prints out these 6 variables
+print end1 + end2 + end3 + end4 + end5 + end6
+
+#this line prints out these variables using put
+puts end7 + end8 + end9 + end10 + end11 + end12
