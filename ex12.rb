8109fb7864d00e38cf5275be55d31a73fa03cfdc
Tommy Chapman <Tommy.chapman@hotmail.com>
2020-12-01 22:06:46 -0600

    worked on more
    cranked out some more exes

---
+++ ex12.rb
@@ -0,0 +1,21 @@
+#print "Give me a number: "
+#number = gets.chomp.to_i
+
+#bigger = number * 100
+#puts "A bigger number is #{bigger}."
+
+#print "Give me another number: "
+#another = gets.chomp
+#number = another.to_i
+
+#smaller = number / 100
+#puts "A smaller number is #{smaller}."
+
+
+# study drill below
+
+print "Give me some money: "
+money = gets.chomp.to_f
+
+change = money / 10
+print "Your change is $#{change}."
